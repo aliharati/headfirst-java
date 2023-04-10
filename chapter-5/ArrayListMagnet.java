ee92396e41cedb9e5d90efd24a2800d723d20e4a
Ali Harati <77158617+aliharati@users.noreply.github.com>
2023-04-10 18:18:23 +0330

    Create ArrayListMagnet.java

---
+++ chapter-5/ArrayListMagnet.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ArrayListMagnet {
+
+    
+    public static void main (String[] args) {
+        ArrayList<String> a = new ArrayList<String>();
+        a.add(0, "zero");
+        a.add(1, "one");
+        a.add(2, "two");
+        printList(a);
+    }
+
+    public static void printList(ArrayList<String> list) {
+
+
+    }
+
+}
+
